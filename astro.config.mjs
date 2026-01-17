673c89caa56a73acc6821085d604edee472d08a2
Miiischwald <luca.ingenbleek@miiischwald.de>
2026-01-17 16:49:29 +0100

    chore: enforce trailingSlash=always for consistent canonicals (#9)
    Co-authored-by: KIrun3000 <luca@ingenbleek.io>

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.miiischwald.de",
+  trailingSlash: "always",
   integrations: [tailwind(), icon()],
 });
